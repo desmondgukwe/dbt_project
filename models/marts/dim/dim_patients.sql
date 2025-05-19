09ea8e24a8104995e362bc20590ffe6c94981761
Desmond Gukwe <gukwedesmond@gmail.com>
2025-05-19 10:39:41 +0000

    created fct table

--- models/marts/dim/dim_patients.sql
+++ models/marts/dim/dim_patients.sql
@@ -9,14 +9,37 @@
 --   - Total claim amount
 --   - Days since first claim
 
+-- models/marts/dim/dim_patients.sql
+
 with patients as (
     select * from {{ ref('raw_patients') }}
 ),
 
--- Add your logic here to join with stg_claims
+claims as (
+    select * from {{ ref('stg_claims') }}
+),
+
+aggregated_claims as (
+    select
+        patient_id,
+        min(claim_date) as first_claim_date,
+        count(*) as total_claims,
+        sum(claim_amount) as total_claim_amount,
+        date_diff(current_date(), min(claim_date), day) as days_since_first_claim
+    from claims
+    group by patient_id
+),
 
 final as (
-    select * from patients
+    select
+        p.*,
+        ac.first_claim_date,
+        ac.total_claims,
+        ac.total_claim_amount,
+        ac.days_since_first_claim
+    from patients p
+    left join aggregated_claims ac
+        on p.patient_id = ac.patient_id
 )
 
 select * from final
